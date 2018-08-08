ec66c0021f80fc18e3f542010a11d320c6baef87
Connor O <connor607@gmail.com>
2018-08-08 13:46:56 -0500

    Change Gatsby theme

---
+++ data/SiteConfig.js
@@ -0,0 +1,42 @@
+﻿module.exports = {
+  siteTitle: "Interesting Things", // Site title.
+  siteTitleShort: "Interesting Things", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "Interesting Things", // Alternative site title for SEO.
+  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
+  siteUrl: "https://inthings.cf", // Domain of your website without pathPrefix.
+  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
+  siteDescription: "It's the cat's meow!", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml", // Path to the RSS file.
+  siteFBAppID: "", // FB Application ID for using app insights
+  siteGATrackingID: "", // Tracking code ID for google analytics.
+  disqusShortname: "", // Disqus shortname.
+  postDefaultCategoryID: "General", // Default category for posts.
+  dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
+  dateFormat: "DD/MM/YYYY", // Date format for display.
+  userName: "Connor O", // Username to display in the author segment.
+  userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Rose Hill, Texas", // User location to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
+  userDescription:
+    "Yeah, I like animals better than people sometimes... Especially cats. However, sometimes cats can be a bit heard headed. My cats can usually be found loafing on the couch...", // User description to display in the author segment.
+  // Links to social profiles/projects you want to display in the author segment/navigation bar.
+  userLinks: [
+    {
+      label: "GitHub",
+      url: "https://github.com/Vagr9K/gatsby-material-starter",
+      iconClassName: "fa fa-github"
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/Vagr9K",
+      iconClassName: "fa fa-twitter"
+    },
+    {
+      label: "Email",
+      url: "mailto:vagr9k@gmail.com",
+      iconClassName: "fa fa-envelope"
+    }
+  ],
+  copyright: "Copyright © 2017. Material User" // Copyright string for the footer of the website and RSS feed.
+};
